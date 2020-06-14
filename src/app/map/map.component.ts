79656bdde99f5bc0c1e2e86b94f040f30bac03cc
Andreas Dohren <dohren@web.de>
2020-06-14 20:02:28 +0200

    GPX Datenformat hinzugefügt

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,7 +2,6 @@ import { Component, HostListener } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import VectorLayer from 'ol/layer/Vector';
-import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
@@ -13,7 +12,11 @@ import Interaction from 'ol/interaction';
 import Overlay from 'ol/overlay';
 import Proj from 'ol/proj';
 import XYZ from 'ol/source/xyz';
-
+import VectorSource from 'ol/source/Vector';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import { Fixed } from 'ol/loadingstrategy';
+import GPX from 'ol/format/GPX';
+import NoSleep from '../../assets/js/NoSleep';
 
 declare var ol: any;
 
@@ -32,6 +35,9 @@ export class MapComponent {
     maximumAge: 0
   };
 
+  noSleep = new NoSleep();
+  
+  navigate:boolean = false;
   map: Map;
   // source: XYZ;
   // layer: TileLayer;
@@ -41,13 +47,67 @@ export class MapComponent {
   // olFeature: Feature;
  
   ngOnInit() {
-
     this.createMap();
     this.showPosition(); 
+    this.track();
+  }
+
+
+  private get(url, callback) {
+    var client = new XMLHttpRequest();
+    client.open('GET', url);
+    client.onload = function() {
+      callback(client.responseText);
+    };
+    client.send();
+  }
+
+
+  private track() {
+    var gpxFormat = new GPX();
     
+    var style = {
+      'Point': new Style({
+        image: new CircleStyle({
+          fill: new Fill({
+            color: 'rgba(255,255,0,0.4)'
+          }),
+          radius: 5,
+          stroke: new Stroke({
+            color: '#f60404 	',
+            width: 1
+          })
+        })
+      }),
+      'LineString': new Style({
+        stroke: new Stroke({
+          color: '#f60404 	',
+          width: 3
+        })
+      }),
+      'MultiLineString': new Style({
+        stroke: new Stroke({
+          color: '#f60404 	',
+          width: 3
+        })
+      })
+    };
+    
+    var vector = new VectorLayer({
+      source: new VectorSource({
+        url: 'assets/example.gpx',
+        format: new GPX()
+      }),
+      style: function(feature) {
+        return style[feature.getGeometry().getType()];
+      }
+    });
+    this.map.addLayer(vector);
+
   }
 
   private showPosition() {
+    
     const geolocation = new ol.Geolocation({
       projection: this.map.getView().getProjection(),
       tracking: true,
@@ -75,9 +135,12 @@ export class MapComponent {
       style: iconStyle
     });
     this.map.addLayer(iconLayer);
+
+
     geolocation.on('change', (event) => {
       this.pos = geolocation.getPosition();
       iconFeature.setGeometry(new ol.geom.Point(this.pos));
+      this.navigation();
     });
   }
 
@@ -89,6 +152,7 @@ export class MapComponent {
 
     this.map = new Map({
       target: 'map',
+      attributionControl: false,
       keyboardEventTarget: document,
       controls: ol.control.defaults({
         attributionOptions: {
@@ -103,6 +167,9 @@ export class MapComponent {
       ],
       view: this.view
     });
+
+    let elements = document.getElementsByClassName('ol-attribution');
+    elements.item(0).setAttribute('style', 'display:none');
   }
 
   @HostListener('document:keydown.softright')
@@ -115,14 +182,34 @@ export class MapComponent {
   onSoftLeft() {
     var view = this.map.getView();
     view.setZoom(view.getZoom()-1);
+    this.navigation();
+  }
+
+
+  navigation() {
+    if (this.navigate) {
+      this.view.setCenter(this.pos);
+      this.view.setZoom(16);
+    } 
   }
 
   @HostListener('document:keydown.enter')
   onEnter() {
     //navigator.geolocation.getCurrentPosition(this.showPosition2, this.error, this.options);
     // id = navigator.geolocation.watchPosition(success[, error[, options]])
-    this.view.setCenter(this.pos);
-    this.view.setZoom(12); 
+    
+    if (this.navigate == false) {
+      alert("starte Naviation")
+      this.navigate = true;
+      this.navigation();
+      this.noSleep.enable();
+    }
+    else {
+      alert("stoppe Navigation");
+      this.navigate = false;
+      this.noSleep.disable();  
+    }
+
   }
 
   error() {
